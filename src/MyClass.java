9bf3e54567cb91fc87c793fb393fc084f57773d8
sai <saimate2001@gmail.com>
2022-11-05 19:09:21 +0530

    main methode and print a

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,6 @@
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("first class");
+        System.out.println("second class");
     }
 }
---
+++ src/newClass.java
@@ -0,0 +1,6 @@
+public class newClass {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("the value of a is "+a);
+    }
+}
