0195cfbffd11954f230e85c490b8597be60918b4
saimate <110829659+saimate@users.noreply.github.com>
2022-11-05 19:14:41 +0530

    Update newClass.java

--- src/newClass.java
+++ src/newClass.java
@@ -1,6 +1,10 @@
 public class newClass {
     public static void main(String[] args) {
         int a = 10;
+        int b = 11;
         System.out.println("the value of a is "+a);
+                System.out.println("the value of a is "+b);
+
+        
     }
 }
