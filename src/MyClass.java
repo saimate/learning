50bbb19c95343dae6a077cbe42811d472d0bf72f
saimate <110829659+saimate@users.noreply.github.com>
2022-11-07 19:16:51 +0530

    Update MyClass.java
    bvv

--- src/MyClass.java
+++ src/MyClass.java
@@ -6,6 +6,17 @@ public class MyClass {
         int b =20;
         int c = a+b;
         System.out.println("The sum is"+c);
+        public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("first class");
+        System.out.println("second class");
+        int a =10;
+        int b =20;
+        int c = a+b;
+        System.out.println("The sum is"+c);
+        
+    }
+}
         
     }
 }
