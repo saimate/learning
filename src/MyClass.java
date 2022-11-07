cf4c327522c1c7ed09140e33deb556337f8fe780
saimate <110829659+saimate@users.noreply.github.com>
2022-11-07 19:09:31 +0530

    Update MyClass.java
    update sum

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,5 +2,10 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("first class");
         System.out.println("second class");
+        int a =10;
+        int b =20;
+        int c = a+b;
+        System.out.println("The sum is"+c);
+        
     }
 }
