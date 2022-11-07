2a78dbaf2a6978d6aaa29a991c64cd917b673fd5
sai <saimate2001@gmail.com>
2022-11-07 19:12:54 +0530

    main methode and print

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,6 +2,7 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("first class");
         System.out.println("second class");
-
+        System.out.println("first cwfw");
+        System.out.println("second cwefw");
     }
 }
